83c5ea8cff3436db47c07695574e714aacbaa42e
Keisuke-Toku-2000 <keisuke20000923@gmail.com>
2021-02-28 00:15:18 +0900

    test

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,45 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <main>
+      <v-btn @click="api()">api</v-btn>
+    </main>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'Home',
+  name: 'App',
+
   components: {
-    HelloWorld
+  },
+
+  data: () => ({
+    //
+  }),
+  methods: {
+      api(){
+        var axios = require('axios');
+        var data = 'grant_type=password&username=qhp4502q5er245@print.epsonconnect.com&password=';
+
+        var config = {
+          method: 'post',
+          url: 'https://api.epsonconnect.com/api/1/printing/oauth2/auth/token?subject=printer',
+          headers: { 
+            'Content-Type': 'application/x-www-form-urlencoded; charset=utf-8', 
+            'Authorization': 'Basic ZmZiMWYwMjhmZDQzNGZhYmFhNWMyYmNjN2VhOWY1NzI6YkN1YTllN1pESlRYcnAwZ3hDYzF3SVpLZTN2NUNnbDZobXBaYW1pdDVPSGVtUUFWTVJRMzRVUGc5aHNPZU04aw=='
+          },
+          data : data
+        };
+
+        axios(config)
+        .then(function (response) {
+          console.log(JSON.stringify(response.data));
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+
+    }
   }
 }
 </script>
