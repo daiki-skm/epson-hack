c5a604e3c93bcd7c77cc46fc17e9eef42543361b
hond0413 <62871691+hond0413@users.noreply.github.com>
2021-02-28 16:48:30 +0900

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <v-app>
     <v-main>
+    <v-btn @click="test()">QRコード化</v-btn>
       <HelloWorld/>
     </v-main>
   </v-app>
@@ -17,7 +18,14 @@ export default {
   },
 
   data: () => ({
-    //
+   getUrl: 'https://photos.app.goo.gl/6nNMr6gmhpHZnT8H8',
+    textareavalue: "",
+    show: true,
   }),
+  methods: {
+    this.textareavalue = "https://chart.googleapis.com/chart?cht=qr&chs=200x200&chl=" + this.getUrl + "&choe=UTF-8QRCode"
+      var link = document.createElement('a')
+      document.getElementById("back").style.backgroundImage = "url(" + this.textareavalue + ");"
+  }
 };
 </script>
