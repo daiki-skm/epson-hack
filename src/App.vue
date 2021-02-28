29d8cccc32351bb0b98387c7fa68e9bd0cb35ed5
佐久間大空 <daikisakuma6040@gmail.com>
2021-02-28 17:07:01 +0900

    comment

--- src/App.vue
+++ src/App.vue
@@ -18,14 +18,13 @@ export default {
   },
 
   data: () => ({
-   getUrl: 'https://photos.app.goo.gl/6nNMr6gmhpHZnT8H8',
+    getUrl: 'https://photos.app.goo.gl/6nNMr6gmhpHZnT8H8',
     textareavalue: "",
     show: true,
   }),
   methods: {
     test () {
       this.textareavalue = "https://chart.googleapis.com/chart?cht=qr&chs=200x200&chl=" + this.getUrl + "&choe=UTF-8QRCode"
-      var link = document.createElement('a')
       document.getElementById("back").style.backgroundImage = "url(" + this.textareavalue + ");"
     }
   }
