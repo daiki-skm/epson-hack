9d9a1d4437b2d8d2b582d3178fdcc2c73ce44bfc
keisuke20000923@gmail.com <hondsoccer0413@gmail.com>
2021-02-28 16:38:23 +0900

    new branch

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,4 +1,5 @@
 <template>
+<<<<<<< HEAD
   <div class="hello">
     <body>
       <img v-if="show" :src="textareavalue"> 
@@ -15,10 +16,105 @@
       </section>
     </body>
   </div>
+=======
+  <v-container>
+    <v-row class="text-center">
+      <v-col cols="12">
+        <v-img
+          :src="require('../assets/logo.svg')"
+          class="my-3"
+          contain
+          height="200"
+        />
+      </v-col>
+
+      <v-col class="mb-4">
+        <h1 class="display-2 font-weight-bold mb-3">
+          Welcome to Vuetify
+        </h1>
+
+        <p class="subheading font-weight-regular">
+          For help and collaboration with other Vuetify developers,
+          <br>please join our online
+          <a
+            href="https://community.vuetifyjs.com"
+            target="_blank"
+          >Discord Community</a>
+        </p>
+      </v-col>
+
+      <v-col
+        class="mb-5"
+        cols="12"
+      >
+        <h2 class="headline font-weight-bold mb-3">
+          What's next?
+        </h2>
+
+        <v-row justify="center">
+          <a
+            v-for="(next, i) in whatsNext"
+            :key="i"
+            :href="next.href"
+            class="subheading mx-3"
+            target="_blank"
+          >
+            {{ next.text }}
+          </a>
+        </v-row>
+      </v-col>
+
+      <v-col
+        class="mb-5"
+        cols="12"
+      >
+        <h2 class="headline font-weight-bold mb-3">
+          Important Links
+        </h2>
+
+        <v-row justify="center">
+          <a
+            v-for="(link, i) in importantLinks"
+            :key="i"
+            :href="link.href"
+            class="subheading mx-3"
+            target="_blank"
+          >
+            {{ link.text }}
+          </a>
+        </v-row>
+      </v-col>
+
+      <v-col
+        class="mb-5"
+        cols="12"
+      >
+        <h2 class="headline font-weight-bold mb-3">
+          Ecosystem
+        </h2>
+
+        <v-row justify="center">
+          <a
+            v-for="(eco, i) in ecosystem"
+            :key="i"
+            :href="eco.href"
+            class="subheading mx-3"
+            target="_blank"
+          >
+            {{ eco.text }}
+          </a>
+        </v-row>
+      </v-col>
+    </v-row>
+  </v-container>
+>>>>>>> 624a02ba1b130a9cd06fe25443fea14a6d8ef498
 </template>
 
 <script>
+  export default {
+    name: 'HelloWorld',
 
+<<<<<<< HEAD
 export default {
   name: 'googlePhoto',
   components:{
@@ -93,99 +189,154 @@ export default {
         }
       })
       .catch(error => {
+=======
+    data: () => ({
+      responsedata:'',
+      ecosystem: [
+        {
+          text: 'vuetify-loader',
+          href: 'https://github.com/vuetifyjs/vuetify-loader',
+        },
+        {
+          text: 'github',
+          href: 'https://github.com/vuetifyjs/vuetify',
+        },
+        {
+          text: 'awesome-vuetify',
+          href: 'https://github.com/vuetifyjs/awesome-vuetify',
+        },
+      ],
+      importantLinks: [
+        {
+          text: 'Documentation',
+          href: 'https://vuetifyjs.com',
+        },
+        {
+          text: 'Chat',
+          href: 'https://community.vuetifyjs.com',
+        },
+        {
+          text: 'Made with Vuetify',
+          href: 'https://madewithvuejs.com/vuetify',
+        },
+        {
+          text: 'Twitter',
+          href: 'https://twitter.com/vuetifyjs',
+        },
+        {
+          text: 'Articles',
+          href: 'https://medium.com/vuetify',
+        },
+      ],
+      whatsNext: [
+        {
+          text: 'Explore components',
+          href: 'https://vuetifyjs.com/components/api-explorer',
+        },
+        {
+          text: 'Select a layout',
+          href: 'https://vuetifyjs.com/getting-started/pre-made-layouts',
+        },
+        {
+          text: 'Frequently Asked Questions',
+          href: 'https://vuetifyjs.com/getting-started/frequently-asked-questions',
+        },
+      ],
+    }),
+     methods: {
+      api(){
+        var axios = require('axios');
+        var data = 'grant_type=password&username=qhp4502q5er245@print.epsonconnect.com&password=';
+        var config = {
+          method: 'post',
+          url: 'https://api.epsonconnect.com/api/1/printing/oauth2/auth/token?subject=printer',
+          headers: { 
+            'Content-Type': 'application/x-www-form-urlencoded; charset=utf-8', 
+            'Authorization': 'Basic ZmZiMWYwMjhmZDQzNGZhYmFhNWMyYmNjN2VhOWY1NzI6YkN1YTllN1pESlRYcnAwZ3hDYzF3SVpLZTN2NUNnbDZobXBaYW1pdDVPSGVtUUFWTVJRMzRVUGc5aHNPZU04aw=='
+          },
+          data : data
+        };
+
+        axios(config)
+        .then(response => {
+          const obj = response.data;
+          this.responsedata = obj;
+          console.log(this.responsedata.access_token)
+        })
+        .catch( error => {
+>>>>>>> 624a02ba1b130a9cd06fe25443fea14a6d8ef498
         console.log(error);
-      });
-    },
-    handleClientLoad () {
-      this.gapi.load('client:auth2', this.initClient);
-    },
-    initClient() {
-      this.gapi.client.init({
-        apiKey: this.apiKey,
-        clientId: this.clientId,
-        discoveryDocs: this.discoveryDocs,
-        scope: this.scopes,
-      }).then(() => {
-        // サインイン状態を監視し、状態に変化があったときに「updateSigninStatus」を呼ぶ
-        this.gapi.auth2.getAuthInstance().isSignedIn.listen(this.updateSigninStatus);
-        // 初期起動時のサインイン状態で画面制御
-        this.updateSigninStatus(this.gapi.auth2.getAuthInstance().isSignedIn.get());
-        this.authorizeButton.onclick = this.handleAuthClick;
-        this.signoutButton.onclick = this.handleSignoutClick;
-      });
-    },
-    updateSigninStatus(isSignedIn) {
-      if (isSignedIn) {
-        this.authorizeButton.style.display = 'none';
-        this.signoutButton.style.display = 'block';
-      } else {
-        this.authorizeButton.style.display = 'block';
-        this.signoutButton.style.display = 'none';
+        });
+        this.api2();
+      },
+      api2(){
+          var axios = require('axios');
+          var data = '{\r\n    "job_name" : "Job Name",\r\n    "print_mode" : "document",\r\n    "print_setting" : {\r\n        "media_size" : "ms_a4",\r\n        "media_type" : "mt_plainpaper",\r\n        "borderless" : false,\r\n        "print_quality" : "normal",\r\n        "source" : "auto",\r\n        "color_mode" : "color",\r\n        "reverse_order" : false,\r\n        "copies" : 1,\r\n        "collate" : false\r\n    }\r\n}';
+          var config = {
+            method: 'post',
+            url: 'https://api.epsonconnect.com/api/1/printing/printers/8f124241fc7046cebbd0abd4545c7672/jobs',
+            headers: { 
+              'Content-Type': 'application/json; charset=utf-8', 
+              'Authorization': this.responsedata.token_type+" "+this.responsedata.access_token
+            },
+            data : data
+          };
+          axios(config)
+          .then(function (response) {
+            console.log(JSON.stringify(response.data));
+          })
+          .catch(function (error) {
+            console.log(error);
+          });
+        this.api3();
+      },
+      api3(){
+        var axios = require('axios');
+        var data = '1.png';
+
+        var config = {
+          method: 'post',
+          url: 'https://www.epsonconnect.com/c33fe124ef80c3b13670be27a6b0bcd7/v1/storage/PostData?Key=ceee70d8d80d64ecef98b9782751b6c6a2fac953783030627f53694863b658ec79f8b2ba024054fd&File=1.pdf',
+          headers: { 
+            'Content-Length': '50589', 
+            'Content-Type': 'application/octet-stream', 
+            'Authorization': this.responsedata.token_type+" "+this.responsedata.access_token
+          },
+          data : data
+        };
+
+        axios(config)
+        .then(function (response) {
+          console.log(JSON.stringify(response.data));
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+        this.api4();
+      },
+      api4(){
+        var axios = require('axios');
+        var data = '1.png';
+
+        var config = {
+          method: 'post',
+          url: 'https://api.epsonconnect.com/api/1/printing/printers/8f124241fc7046cebbd0abd4545c7672/jobs/89d5f64845e94387907bc9ba22a84ad4/print',
+          headers: { 
+            'Content-Type': '0', 
+            'Authorization': this.responsedata.token_type+" "+this.responsedata.access_token
+          },
+          data : data
+        };
+
+        axios(config)
+        .then(function (response) {
+          console.log(JSON.stringify(response.data));
+        })
+        .catch(function (error) {
+          console.log(error.response);
+        });
+
       }
-    },
-    handleAuthClick() {
-      this.gapi.auth2.getAuthInstance().signIn();
-    },
-    handleSignoutClick() {
-      this.gapi.auth2.getAuthInstance().signOut();
-      this.gapi.auth2.getAuthInstance().disconnect();
-    },
-    previewFiles() {
-      this.files = this.$refs.myFiles.files
-      console.log(this.files[0])
-      this.upload(this.files[0])
-    },
-    // 選択したファイルをGoogleフォトへアップルードする
-    upload(file) {
-      var accessToken = this.gapi.auth.getToken().access_token; // OAuthアクセスキーを取得
-
-      // Media Upload APIでファイルをアップロード
-      fetch('https://photoslibrary.googleapis.com/v1/uploads', {
-        method: 'POST',
-        headers: new Headers({ 
-          'Authorization': 'Bearer ' + accessToken,
-          "Content-type": "application/octet-stream",
-          "X-Goog-Upload-Content-Type": file.type,
-          "X-Goog-Upload-Protocol": "raw"
-        }),
-        body: file,
-      })
-      .then(res => res.text())
-      .then(token => {
-        // レスポンスのUploadTokenをパラメータにbatchCreateを呼ぶ
-        return this.batchCreate(token);
-      })
-      .then(() => {
-        this.pullPhotoUrl()
-      });
-    },
-    // Media Batch Create APIを呼び出し、Googleフォトへアップロードしたファイルを登録
-    batchCreate(uploadToken) {
-      // gapiで、Media Batch Create API呼び出しリクエストの作成
-      var restRequest = this.gapi.client.request({
-        'path': 'https://photoslibrary.googleapis.com/v1/mediaItems:batchCreate',
-        'method': 'POST',
-        'body': {
-          'newMediaItems' : [
-            {
-              'description': "test batch create", // ファイルの概要
-              'simpleMediaItem': {
-                'uploadToken': uploadToken,  // Media Upload APIのレスポンスで取得したUploadTokenをセット
-                'fileName': 'test file name' // ファイル名
-              }
-            }
-          ]
-        }
-      });
-      // リクエストの実行
-      restRequest.execute((resp) => {
-        console.log(resp);
-      });
-    },
   }
-}
+  }
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-</style>
