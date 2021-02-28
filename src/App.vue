96f481cbe7b9ca31e5750638fbd5954435923100
佐久間大空 <daikisakuma6040@gmail.com>
2021-02-28 17:15:12 +0900

    comment

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,8 @@
   <v-app>
     <v-main>
     <v-btn @click="test()">QRコード化</v-btn>
+    <img v-if="show" :src="textareavalue"> 
+    <v-text-field v-model="getUrl" placeholder="input here"></v-text-field>
       <HelloWorld/>
     </v-main>
   </v-app>
@@ -12,11 +14,9 @@ import HelloWorld from './components/HelloWorld';
 
 export default {
   name: 'App',
-
   components: {
     HelloWorld,
   },
-
   data: () => ({
     getUrl: 'https://photos.app.goo.gl/6nNMr6gmhpHZnT8H8',
     textareavalue: "",
@@ -25,7 +25,6 @@ export default {
   methods: {
     test () {
       this.textareavalue = "https://chart.googleapis.com/chart?cht=qr&chs=200x200&chl=" + this.getUrl + "&choe=UTF-8QRCode"
-      document.getElementById("back").style.backgroundImage = "url(" + this.textareavalue + ");"
     }
   }
 };
