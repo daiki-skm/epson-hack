ec8349590be80ba5e412c4e155b8361fba8ab2c7
hond0413 <62871691+hond0413@users.noreply.github.com>
2021-02-28 17:02:00 +0900

    Update Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="home">
     <HelloWorld />
+    <v-btn @click="test()">TEST</v-btn>
+    <img v-if="show" :src="textareavalue"> 
     <v-text-field v-model="getUrl" placeholder="input here"></v-text-field>
   </div>
   
@@ -13,6 +15,16 @@ export default {
   name: 'Home',
   components: {
     HelloWorld
+  }, 
+  data: ()=> ({
+    getUrl: 'https://photos.app.goo.gl/6nNMr6gmhpHZnT8H8',
+    textareavalue: "",
+    show: true,
+  }),
+  methods:{
+    test(){
+      this.textareavalue = "https://chart.googleapis.com/chart?cht=qr&chs=200x200&chl=" + this.getUrl + "&choe=UTF-8QRCode"  
+    }
   }
 }
 </script>
